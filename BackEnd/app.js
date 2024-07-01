a58ee640a9f970c83a896f0d75c1da199aa925e8
lobna3 <yousfi55lobna@gmail.com>
2024-07-01 22:24:25 +0200

    create server app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,1 +1,20 @@
-console.log("hello")
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+const port = 5000
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello new Projekt")
+})
+
+
+
+app.listen(port,()=>{
+   console.log(`server listenning on port ${port}`)
+})
