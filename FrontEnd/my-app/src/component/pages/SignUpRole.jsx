3db71361675dd495ef78f436a1449e197bd46470
achrefmehri <ashmaymh@gmail.com>
2024-07-04 13:06:53 +0100

    start working on freelancer interface and client interface

--- FrontEnd/my-app/src/component/pages/SignUpRole.jsx
+++ FrontEnd/my-app/src/component/pages/SignUpRole.jsx
@@ -11,21 +11,23 @@ const SignUpRole = (props) => {
             <h3 className='text-3xl font-bold '>Join as a client or freelancer</h3>
         </div>
         <div className="role-choosing flex mt-10 gap-10">
-        <div className="client border-4 p-2 rounded-xl cursor-pointer hover:border-[#108a00]" onClick={()=>props.setRole('client')}>
+        <div className="client border-4 p-2 rounded-xl cursor-pointer hover:border-[#108a00]" >
             <div className="cont flex flex-row justify-between">
             <img src ={ClientIcon} className='h-10 ml-4'></img>
             <label class="container ml-24 mt-2  ">
-            <input  type="checkbox"  className='hidden'/>
-            <div class="checkmark"></div>
-            </label>            </div>
+            
+            <input  type="checkbox" name='inpt'  className='hidden'/>
+            <div class="checkmark" onClick={()=>props.setSignUpRole('client')}></div>
+            </label>           
+             </div>
           <h3 className='text-xl font-semibold mt-4'>I’m a client,<br></br> hiring for a<br></br> project</h3>
         </div>
-        <div className="freelancer border-4 rounded-xl p-2 hover:border-[#108a00] cursor-pointer" onClick={()=>props.setRole('freelancer')}>
+        <div className="freelancer border-4 rounded-xl p-2 hover:border-[#108a00] cursor-pointer" >
             <div className="cont flex flex-row justify-between">
             <img src ={FreelancerIcon} className='h-10 ml-4'></img>
             <label class="container ml-24 mt-2 ">
-            <input  type="checkbox"  className='hidden'/>
-            <div class="checkmark"></div>
+            <input  type="checkbox" name='inpt'  className='hidden'/>
+            <div class="checkmark" onClick={()=>props.setSignUpRole('freelancer')}></div>
             </label>
             </div>
           <h3 className='text-xl mt-4 font-semibold'>I’m a freelancer,<br></br> looking for work</h3>
