e7a88cc0f050c39b49676a1e43b8d7ef1235941d
lobna3 <yousfi55lobna@gmail.com>
2024-07-04 12:01:23 +0200

    Set up Multer for image uploads

--- BackEnd/middleware/multer.js
+++ BackEnd/middleware/multer.js
@@ -1,11 +1,5 @@
-const multer = require('multer');
+const multer = require('multer')
+const storage = multer.memoryStorage()  // store image in memory
+const upload = multer({storage:storage})
 
-const storage = multer.diskStorage({
-  filename: function (req,file,cb) {
-    cb(null, file.originalname)
-  }
-});
-
-const upload = multer({storage: storage});
-
-module.exports = upload;
+module.exports = upload
